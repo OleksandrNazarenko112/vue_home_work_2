bef6d9e55bfbf02705124f3c0d79e4357dccb351
OleksandrNazarenko112 <nazarenkovideo@gmail.com>
2017-11-01 15:08:45 +0200

    update-json

--- vue.js
+++ vue.js
@@ -1,6 +1,6 @@
 const Content = ( 'the-content', {
   props:["info"],
-    template:`<h1>{{info}}</h1>`
+    template:`<div><h1 v-for="item in info">{{ item }}</h1></div>`
 })
 
 
@@ -8,19 +8,29 @@ const Content = ( 'the-content', {
 var app = new Vue({
   el: '#app',
   data:{
-    text: "test",
-    sourceURL:"https://garevna.github.io/vue.github.io/data/posts.json"
+    text: {},
+    sourceURL:"https://oleksandrnazarenko112.github.io/vue_home_work_2/test.json"
   },
     components: {
     'the-content': Content
   },
+    computed: {
+    titles: function () {
+
+      return Object.keys ( this.text )
+
+    },
+  },
+
     methods: {
     loadJSON: function () {
       this.$http.get( this.sourceURL ).then (response => {
                         this.text = response.body
                     }, response => {
                         console.log ("Ошибка доступа к файлу: " + this.sourceURL)
+
       })
+         console.log(this.titles)
     }
   }
 
